b75e8189c7af30ef97626f9a1634ed1d28df80ca
Sophia Ferreira <f1783574@gmail.com>
2026-09-20 20:41:16 -0300

    chore: adiciona esqueleto inicial da aplicação e teste de smoke

---
+++ src/main/java/com/projeto/acessopuc/AcessopucApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.acessopuc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcessopucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcessopucApplication.class, args);
+	}
+
+}
